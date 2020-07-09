e23e994a705b618d508dd3b506156468b5b8f752
Mateus Santos <mateus.gcr.tog@hotmail.com>
2020-07-09 20:53:11 -0300

    Criado Entities e Classes de composição do projeto

---
+++ ProjetoValorTrabalhado/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoValorTrabalhado.Entities.Enums
+{
+    class WorkerLevel
+    {
+    }
+}
---
+++ ProjetoValorTrabalhado/Entities/Worker.cs
@@ -0,0 +1,13 @@
+﻿using ProjetoValorTrabalhado.Entities.Enums;
+
+
+namespace ProjetoValorTrabalhado.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departent Departent { get; set; }
+    }
+}
